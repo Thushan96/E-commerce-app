3516b6ba45b448e8c74b8795f3663dc741c27924
Thushan96 <thushanmadhulakshitha@gmail.com>
2024-03-12 17:03:58 +0530

    added data

--- src/services/data.ts
+++ src/services/data.ts
@@ -1,2 +1,36 @@
-// data.ts
-export {};
+export const products = [
+    {
+      productId: "1",
+      name: "Product 1",
+      quantity: 10,
+      price: 9.99,
+      images: [
+        "https://example.com/product1-image1.jpg",
+        "https://example.com/product1-image2.jpg",
+        "https://example.com/product1-image3.jpg"
+      ]
+    },
+    {
+      productId: "2",
+      name: "Product 2",
+      quantity: 20,
+      price: 19.99,
+      images: [
+        "https://example.com/product2-image1.jpg",
+        "https://example.com/product2-image2.jpg",
+        "https://example.com/product2-image3.jpg"
+      ]
+    },
+    {
+      productId: "3",
+      name: "Product 3",
+      quantity: 15,
+      price: 14.99,
+      images: [
+        "https://example.com/product3-image1.jpg",
+        "https://example.com/product3-image2.jpg",
+        "https://example.com/product3-image3.jpg"
+      ]
+    }
+  ];
+  
